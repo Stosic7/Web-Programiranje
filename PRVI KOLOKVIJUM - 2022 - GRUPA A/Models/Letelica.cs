d7ef29980aa74de72a2e3f6303cb999fbba20731
Luka Stošić <158442187+Stosic7@users.noreply.github.com>
2025-07-31 00:01:59 +0200

    Add files via upload

---
+++ PRVI KOLOKVIJUM - 2022 - GRUPA A/Models/Letelica.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace WebTemplate.Models
+{
+    public class Letelica
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public required string Naziv { get; set; }
+
+        public int KapacitetPutnika { get; set; }
+
+        public int BrojOsoblja { get; set; }
+
+        public int BrojMotora { get; set; }
+    }
+}
